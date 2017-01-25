fafc924f77673e0698af34f30ddeb10c67f3192a
Gary Borton <gary.borton@airbnb.com>
2017-01-24 18:11:45 -0800

    Conditionally use es2015 preset when passed options with modules: false.
    Passing the option `modules: false` defeats the purpose of the `without-strict`
    preset, as strict is an option being applied to a plugin that `modules: false`
    removes.  This change will enable users to transform `import` and `import()`
    statements with webpack2 instead of babel, and will enable tree shaking.

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-module.exports = {
-  presets: [
-    require('babel-preset-es2015-without-strict'),
-    require('babel-preset-react'),
-  ],
-  plugins: [
-    [require('babel-plugin-transform-es2015-template-literals'), { spec: true }],
-    require('babel-plugin-transform-es3-member-expression-literals'),
-    require('babel-plugin-transform-es3-property-literals'),
-    require('babel-plugin-transform-jscript'),
-    require('babel-plugin-transform-exponentiation-operator'),
-    require('babel-plugin-syntax-trailing-function-commas'),
-    [require('babel-plugin-transform-object-rest-spread'), { useBuiltIns: true }],
-  ],
-};
+module.exports = function(context, options) {
+  var es2015preset;
+  if (options && options.modules === false) {
+    es2015preset = ['es2015', { modules: false }];
+  } else {
+    es2015preset = 'es2015-without-strict';
+  }
+
+  return {
+    presets: [
+      es2015preset,
+      'react',
+    ],
+    plugins: [
+      ['transform-es2015-template-literals', { spec: true }],
+      'transform-es3-member-expression-literals',
+      'transform-es3-property-literals',
+      'transform-jscript',
+      'transform-exponentiation-operator',
+      'syntax-trailing-function-commas',
+      ['transform-object-rest-spread', { useBuiltIns: true }],
+    ],
+  };
+}

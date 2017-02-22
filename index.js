af669be092f7421fbb1081a39cdcbad5c5c9a5bc
Jordan Gensler <jordan.gensler@airbnb.com>
2017-02-22 12:06:26 -0800

    Adding development plugins and using builtins for jsx spread

--- index.js
+++ index.js
@@ -1,25 +1,53 @@
 'use strict';
 
 module.exports = function buildAirbnbPreset(context, options) {
+  // This is similar to how `env` works in Babel:
+  // https://babeljs.io/docs/usage/babelrc/#env-option
+  // We are not using `env` because it’s ignored in versions > babel-core@6.10.4:
+  // https://github.com/babel/babel/issues/4539
+  // https://github.com/facebookincubator/create-react-app/issues/720
+  // It’s also nice that we can enforce `NODE_ENV` being specified.
+  var env = process.env.BABEL_ENV || process.env.NODE_ENV;
+  if (env !== 'development' && env !== 'test' && env !== 'production') {
+    throw new Error(
+      'Using `babel-preset-airbnb` requires that you specify `NODE_ENV` or '+
+      '`BABEL_ENV` environment variables. Valid values are "development", ' +
+      '"test", and "production". Instead, received: ' + JSON.stringify(env) + '.'
+    );
+  }
+
+  var allPlugins = [
+    [require('babel-plugin-transform-es2015-template-literals'), { spec: true }],
+    require('babel-plugin-transform-es3-member-expression-literals'),
+    require('babel-plugin-transform-es3-property-literals'),
+    require('babel-plugin-transform-jscript'),
+    require('babel-plugin-transform-exponentiation-operator'),
+    require('babel-plugin-syntax-trailing-function-commas'),
+    [require('babel-plugin-transform-object-rest-spread'), { useBuiltIns: true }],
+    [require('babel-plugin-transform-react-jsx'), { useBuiltIns: true }],
+  ];
   var preset; // eslint-disable-line no-var
+
   if (options && options.modules === false) {
     preset = require('babel-preset-es2015').buildPreset(null, { modules: false });
   } else {
     preset = require('babel-preset-es2015-without-strict');
   }
+
+  if (env === 'development') {
+    allPlugins.push(
+      // Adds component stack to warning messages
+      require.resolve('babel-plugin-transform-react-jsx-source'),
+      // Adds __self attribute to JSX which React will use for some warnings
+      require.resolve('babel-plugin-transform-react-jsx-self')
+    );
+  }
+
   return {
     presets: [
       preset,
       require('babel-preset-react'),
     ],
-    plugins: [
-      [require('babel-plugin-transform-es2015-template-literals'), { spec: true }],
-      require('babel-plugin-transform-es3-member-expression-literals'),
-      require('babel-plugin-transform-es3-property-literals'),
-      require('babel-plugin-transform-jscript'),
-      require('babel-plugin-transform-exponentiation-operator'),
-      require('babel-plugin-syntax-trailing-function-commas'),
-      [require('babel-plugin-transform-object-rest-spread'), { useBuiltIns: true }],
-    ],
+    plugins: allPlugins,
   };
 };

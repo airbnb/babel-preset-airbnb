a3b73f66e440b6998642aaa5d0ab60c562cb37cd
Josh Perez <josh.perez@airbnb.com>
2017-06-15 12:38:25 -0700

    Switches to use babel-preset-env
    Babel7 is set to deprecate the ES2015 preset and the new recommended
    approach is to use babel-preset-env which also lets you fine tune the
    transforms specific to the environments the code will be run on. This
    commit moves our implementation to use babel-preset-env as well as lists
    a few browsers we support.

--- index.js
+++ index.js
@@ -1,25 +1,51 @@
 'use strict';
 
+const modules = [require('babel-plugin-transform-es2015-modules-commonjs'), {
+  strict: false,
+}];
+
+const defaultTargets = {
+  android: 30,
+  chrome: 35,
+  edge: 14,
+  explorer: 9,
+  firefox: 52,
+  safari: 8,
+  ucandroid: 1,
+};
+
+function buildTargets(options) {
+  return Object.assign({}, defaultTargets, options.additionalTargets);
+}
+
 module.exports = function buildAirbnbPreset(context, options) {
-  var preset; // eslint-disable-line no-var
-  if (options && options.modules === false) {
-    preset = require('babel-preset-es2015').buildPreset(null, { modules: false });
-  } else {
-    preset = require('babel-preset-es2015-without-strict');
-  }
+  const targets = (options && options.targets) || buildTargets(options);
+
   return {
     presets: [
-      preset,
+      require('babel-preset-env').default(null, {
+        debug: true,
+        exclude: [
+          'transform-async-to-generator',
+          'transform-es2015-template-literals',
+          'transform-regenerator',
+        ],
+        modules: false,
+        targets,
+      }),
       require('babel-preset-react'),
     ],
     plugins: [
-      [require('babel-plugin-transform-es2015-template-literals'), { spec: true }],
+      options && options.modules === false ? null : modules,
+      [require('babel-plugin-transform-es2015-template-literals'), {
+        spec: true,
+      }],
       require('babel-plugin-transform-es3-member-expression-literals'),
       require('babel-plugin-transform-es3-property-literals'),
       require('babel-plugin-transform-jscript'),
-      require('babel-plugin-transform-exponentiation-operator'),
-      require('babel-plugin-syntax-trailing-function-commas'),
-      [require('babel-plugin-transform-object-rest-spread'), { useBuiltIns: true }],
-    ],
+      [require('babel-plugin-transform-object-rest-spread'), {
+        useBuiltIns: true,
+      }],
+    ].filter(Boolean),
   };
 };

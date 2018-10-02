39df3d6dedb18227d4f8fbbadffa94d5300129b0
vikr01 <vik120@icloud.com>
2018-10-01 19:52:10 -0700

    [Breaking] drop support for node <= 4, per babel 7

--- index.js
+++ index.js
@@ -1,64 +1,62 @@
 'use strict';
 
-var assign = require('object.assign');
-
-var modules = [require('babel-plugin-transform-es2015-modules-commonjs'), {
+const modules = [require('babel-plugin-transform-es2015-modules-commonjs'), {
   strictMode: true, // add "use strict"
-  strict: false // this allows __esModule to be exported
+  strict: false, // this allows __esModule to be exported
 }];
 
-var defaultTargets = {
+const defaultTargets = {
   android: 30,
   chrome: 35,
   edge: 14,
   explorer: 9,
   firefox: 52,
   safari: 8,
-  ucandroid: 1
+  ucandroid: 1,
 };
 
 function buildTargets(options) {
-  return assign({}, defaultTargets, options.additionalTargets);
+  return Object.assign({}, defaultTargets, options.additionalTargets);
 }
 
 module.exports = function buildAirbnbPreset(context, options) {
-  var transpileTargets = (options && options.targets) || buildTargets(options || {});
+  const transpileTargets = (options && options.targets) || buildTargets(options || {});
 
-  var debug = (options && typeof options.debug === 'boolean') ? !!options.debug : false;
+  const debug = (options && typeof options.debug === 'boolean') ? !!options.debug : false;
 
   return {
     presets: [
       require('babel-preset-env').default(null, {
-        debug: debug,
+        debug,
         exclude: [
           'transform-async-to-generator',
           'transform-es2015-template-literals',
-          'transform-regenerator'
+          'transform-regenerator',
         ],
         modules: false,
-        targets: transpileTargets
+        targets: transpileTargets,
       }),
-      require('babel-preset-react')
+      require('babel-preset-react'),
     ],
     plugins: [
-      options && !!options.removePropTypes ? ['babel-plugin-transform-react-remove-prop-types', assign({
+      options && !!options.removePropTypes ? ['babel-plugin-transform-react-remove-prop-types', Object.assign({
         mode: 'wrap',
         additionalLibraries: ['airbnb-prop-types'],
-        ignoreFilenames: ['node_modules']
+        ignoreFilenames: ['node_modules'],
       }, options.removePropTypes)] : null,
 
       options && options.modules === false ? null : modules,
       options && options.modules === false ? null : ['babel-plugin-transform-strict-mode', { strictMode: true }],
       [require('babel-plugin-transform-es2015-template-literals'), {
-        spec: true
+        spec: true,
       }],
       require('babel-plugin-transform-es5-property-mutators'),
       require('babel-plugin-transform-es3-member-expression-literals'),
       require('babel-plugin-transform-es3-property-literals'),
       require('babel-plugin-transform-jscript'),
       [require('babel-plugin-transform-object-rest-spread'), {
-        useBuiltIns: true
-      }]
-    ].filter(Boolean)
+        useBuiltIns: true,
+      }],
+    ].filter(Boolean),
   };
 };
